ac705ec0c48bdcba42169bc07e5e3238af746c1c
Michiel de Smet <Michieldesmet1994@gmail.com>
2021-04-05 20:49:53 +0200

    #3 - added code coverage

--- application/infra-jdbc/src/test/kotlin/polar/bear/dashboard/CalculatorInfraJdbcTest.kt
+++ application/infra-jdbc/src/test/kotlin/polar/bear/dashboard/CalculatorInfraJdbcTest.kt
@@ -6,31 +6,31 @@ import org.junit.jupiter.api.Test
 
     @Test
     fun add() {
-        val result = CalculatorCommon().add(10, 10)
+        val result = CalculatorInfraJdbc().add(10, 10)
         assert(result == 20)
     }
 
     @Test
     fun subtract() {
-        val result = CalculatorCommon().subtract(10, 5)
+        val result = CalculatorInfraJdbc().subtract(10, 5)
         assert(result == 5)
     }
 
     @Test
     fun `subtract first check`() {
-        val result = CalculatorCommon().subtract(0, 5)
+        val result = CalculatorInfraJdbc().subtract(0, 5)
         assert(result == 0)
     }
 
     @Test
     fun `Multiply correctly`() {
-        val result = CalculatorCommon().multiply(2, 10)
+        val result = CalculatorInfraJdbc().multiply(2, 10)
         assert(result == 20)
     }
 
     @Test
     fun divide() {
-        val result = CalculatorCommon().divide(10, 2)
+        val result = CalculatorInfraJdbc().divide(10, 2)
         assert(result == 5)
     }
 }
