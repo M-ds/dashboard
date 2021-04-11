f26f22566e524cc1064a7d07b94616ec5c61e528
Michiel de Smet <Michieldesmet1994@gmail.com>
2021-04-11 09:52:52 +0200

    #4 - Simple setup with login and create new user

---
+++ application/frontend/src/components/Login.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="login-container">
+    <h1>Login</h1>
+    <div>
+      <label>
+        Username
+        <input type="text"
+               name="userName"
+               v-model="input.userName"
+               placeholder="Username"
+        />
+      </label>
+      <br/><br/>
+      <label>
+        Password
+        <input type="password"
+               name="password"
+               v-model="input.password"
+               placeholder="Password"
+        />
+      </label>
+    </div>
+    <br><br>
+    <button @click="login()">Log in</button>
+    <br><br>
+    <router-link :to="{name: 'NewUser'}">Create new User</router-link>
+    <!-- Create new user -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      input: {
+        userName: "",
+        password: ""
+      }
+    }
+  },
+  methods: {
+    login: function() {
+      const message = `Username: ${this.input.userName} & Password: ${this.input.password}`;
+      alert(message);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ application/frontend/src/components/NewUser.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <h1>Create new user</h1>
+    <label>
+      Username
+      <input type="text"
+             name="userName"
+             v-model="user.userName"
+             placeholder="Username"
+      />
+    </label>
+    <br><br>
+    <label>
+      Email
+      <input type="text"
+             name="email"
+             v-model="user.email"
+             placeholder="example@email.com"
+      />
+    </label>
+    <br><br>
+    <label>
+      Password
+      <input type="text"
+             name="password"
+             v-model="user.password"
+             placeholder="Password"
+      />
+    </label>
+    <br><br>
+    <label>
+      Repeat password
+      <input type="text"
+             name="passwordConformation"
+             v-model="user.passwordConformation"
+             placeholder="Password"
+      />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NewUser",
+  data() {
+    return {
+      user: {
+        userName: "",
+        email: "",
+        password: "",
+        passwordConformation: "",
+      }
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
