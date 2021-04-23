755f7fea9141fe58412aea366d4c0868b9523d86
Michiel de Smet <Michieldesmet1994@gmail.com>
2021-04-23 13:53:14 +0200

    #10 - Created some unit tests + cleaned up some code

--- application/core/src/test/kotlin/polar/bear/dashboard/user/usecase/UserProfileUseCaseTest.kt
+++ application/core/src/test/kotlin/polar/bear/dashboard/user/usecase/UserProfileUseCaseTest.kt
@@ -1,6 +1,7 @@
 package polar.bear.dashboard.user.usecase
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import polar.bear.dashboard.impl.UserProfileUseCaseImpl
 import polar.bear.dashboard.repository.user.MockUserRepository
 import polar.bear.dashboard.user.domain.UserProfile
@@ -14,9 +15,10 @@ internal class UserProfileUseCaseTest {
     private val mockUserRepository = UserProfileUseCaseTestSetup.mockUserRepository
     private val underTest = userProfileUseCaseImpl()
     private val userProfile = UserProfile("TestUser", "TestPassword", "Test@mail.com")
+    private val emptyUserProfile = UserProfile("", "", "")
 
     @Test
-    fun getUserProfile() {
+    fun `Happy flow of getting a user profile`() {
         GIVEN
         mockUserRepository.getUser(userProfile)
         val expectedResult = userProfile
@@ -26,7 +28,27 @@ internal class UserProfileUseCaseTest {
 
         THEN
         assert(expectedResult == result)
+    }
 
+    @Test
+    fun `No Userprofile is found, thus empty result is returned`() {
+        GIVEN
+        mockUserRepository.getUser(emptyUserProfile)
+        val expectedResult = emptyUserProfile
+
+        WHEN
+        val result = underTest.getUserProfile("100")
+
+        THEN
+        assert(expectedResult == result)
+    }
+
+    @Test
+    fun `Error is thrown because of invalid userId`() {
+        WHEN
+        assertThrows<NullPointerException> {
+            underTest.getUserProfile("ABC")
+        }
     }
 }
 
