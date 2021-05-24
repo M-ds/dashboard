941bb2c606cb02767d03e454efd6d711e4049bd4
Michiel de Smet <Michieldesmet1994@gmail.com>
2021-05-24 10:39:39 +0200

    #18 - Validate the signup request for invalid values

--- application/core/src/main/kotlin/polar/bear/dashboard/person/impl/SignupUseCaseImpl.kt
+++ application/core/src/main/kotlin/polar/bear/dashboard/person/impl/SignupUseCaseImpl.kt
@@ -16,6 +16,15 @@ class SignupUseCaseImpl(
 
     override fun signup(request: SignupUseCase.Request): SignupUseCase.Response {
 
+        val validRequest = validateRequest(request)
+        if (!validRequest.valid) {
+            return SignupUseCase.Response(
+                signupResponse = null,
+                valid = false,
+                errorMessage = validRequest.errorMessage!!
+            )
+        }
+
         val usernameAlreadyExists = personRepository.usernameExists(request.username)
         if (usernameAlreadyExists) {
             return SignupUseCase.Response(
@@ -47,6 +56,7 @@ class SignupUseCaseImpl(
         )
 
         val success = personRepository.save(person)
+
         return if (success) {
             SignupUseCase.Response(
                 signupResponse = SignupResponse(
@@ -64,4 +74,41 @@ class SignupUseCaseImpl(
             )
         }
     }
+
+    private fun validateRequest(request: SignupUseCase.Request): ValidatedRequestResponse {
+        val equalPasswords = checkPasswordForEquality(request.password, request.repeatedPassword)
+        if (!equalPasswords) {
+            return ValidatedRequestResponse(
+                valid = false,
+                errorMessage = "Passwords are not equal!"
+            )
+        }
+        val usernameIsNotEmpty = request.username.isNotBlank()
+        if (!usernameIsNotEmpty) {
+            return ValidatedRequestResponse(
+                valid = false,
+                errorMessage = "Username has to be filled in!"
+            )
+        }
+        val emailAddressIsNotEmpty = request.email.isNotBlank()
+        if (!emailAddressIsNotEmpty) {
+            return ValidatedRequestResponse(
+                valid = false,
+                errorMessage = "Email address has to be filled in!"
+            )
+        }
+        return ValidatedRequestResponse(
+            valid = true,
+            errorMessage = null
+        )
+    }
+
+    private fun checkPasswordForEquality(password: String, repeatedPassword: String): Boolean {
+        return password == repeatedPassword
+    }
 }
+
+data class ValidatedRequestResponse(
+    val valid: Boolean,
+    val errorMessage: String?
+)
